6188f91e726a2ebd7770a5eb4ca774b1b02927c6
arifanf <79965177+arifanf@users.noreply.github.com>
2022-03-30 20:11:24 +0700

    Styling dengan module css

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -8,13 +8,27 @@ import Tomato from './Tomato'
 
 const Burger = () => {
 
+  const ingredients = ['patty', 'lettuce', 'bun', 'cheese', 'patty', 'tomato', 'cheese', 'bun', 'lettuce', 'patty']
+
   return (
     <>
-        <Bun />
-        <Patty />
-        <Lettuce />
-        <Cheese />
-        <Tomato />
+        <Bun type="top" />
+        { ingredients.map( (item, index) => {
+          switch (item) {
+            case 'patty':
+              return <Patty key={index} />
+            case 'lettuce':
+              return <Lettuce key={index} />
+            case 'bun':
+              return <Bun type="insert" key={index} />
+            case 'cheese':
+              return <Cheese key={index} />
+            case 'tomato':
+              return <Tomato key={index} />
+            default:
+              return null
+          }
+        } ) }
         <Bun />
     </>
   )
